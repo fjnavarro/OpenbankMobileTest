572987c2423ce0994d0c4ab3ff3b4153ca3d582a
Fran Navarro <fran@atalayasoft.com>
2020-04-28 14:28:23 +0300

    Add base project

---
+++ OpenbankMobileTest/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  OpenbankMobileTest
+//
+//  Created by Francisco José Navarro García on 28/04/2020.
+//  Copyright © 2020 AtalayaSoft OÜ. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
